2eb2153a2934d3e7f23aa7e3a34f36c747ba8bdd
patkitch <patkitch@aol.com>
2025-10-22 10:46:34 -0500

    fix: add STATIC_ROOT for collectstatic

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -79,8 +81,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # Use DATABASE_URL if present; else SQLite in /app dir
-    "default": env.db(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+      "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pforbes_books",
+        "USER": "pat",
+        "PASSWORD": "Ft3x21,,",  # "" if you didn’t set one
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }  
 }
 
 
@@ -117,6 +125,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = 'static/'
 if "runserver" in sys.argv:
