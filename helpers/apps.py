1d42e5e9fc09b427fbe0de3e31bb4823dcef8d33
patkitch <patkitch@aol.com>
2025-10-24 15:00:04 -0500

    fix: set AppConfig.path for helpers to avoid multiple filesystem locations on DO

--- helpers/apps.py
+++ helpers/apps.py
@@ -1,6 +1,10 @@
 ﻿from django.apps import AppConfig
+import os
 
 class HelpersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "helpers"
     verbose_name = "Helpers"
+
+    # Tell Django "THIS is the canonical location of the helpers app."
+    path = os.path.dirname(os.path.abspath(__file__))
