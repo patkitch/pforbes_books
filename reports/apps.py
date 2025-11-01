a1b19535e37be12553f785de01e40c3efbbe7080
patkitch <patkitch@aol.com>
2025-11-01 15:13:20 -0500

    Fix DigitalOcean build error by adding path to ReportsConfig

--- reports/apps.py
+++ reports/apps.py
@@ -1,8 +1,10 @@
-﻿# reports/apps.py
-from django.apps import AppConfig
+﻿from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
+import os
 
 class ReportsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "reports"
     verbose_name = _("Reports")
+    path = os.path.dirname(os.path.abspath(__file__))  # 👈 tell Django where this app lives
+
