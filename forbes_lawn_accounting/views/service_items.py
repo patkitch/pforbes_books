7a927127ee17c1911cad3ced0c1d5a9333b23504
patkitch <patkitch@aol.com>
2026-01-02 08:40:59 -0600

    Filter service item revenue accounts to Forbes Lawn entity only

--- forbes_lawn_accounting/views/service_items.py
+++ forbes_lawn_accounting/views/service_items.py
@@ -39,9 +39,37 @@ class ServiceItemCreateView(CreateView):
     fields = ['name', 'description', 'category_name', 'default_rate', 'taxable', 'revenue_account', 'active']
     success_url = reverse_lazy('forbes_lawn_accounting:service_item_list')
     
+    def get_form(self, form_class=None):
+        """Filter revenue account dropdown to Forbes Lawn entity only"""
+        form = super().get_form(form_class)
+        
+        # Get Forbes Lawn entity
+        forbes_entity = EntityModel.objects.filter(
+            name__icontains='Forbes Lawn Spraying'
+        ).first()
+        
+        if forbes_entity:
+            # Filter revenue accounts to this entity and revenue accounts (4024/4025)
+            from django_ledger.models import AccountModel
+            form.fields['revenue_account'].queryset = AccountModel.objects.filter(
+                coa_model__entity=forbes_entity,
+                code__in=['4024', '4025']
+            ).order_by('code')
+        
+        return form
+    
     def form_valid(self, form):
-        # Auto-set entity
-        form.instance.entity = EntityModel.objects.first()
+        # Auto-set Forbes Lawn entity
+        forbes_entity = EntityModel.objects.filter(
+            name__icontains='Forbes Lawn Spraying'
+        ).first()
+        
+        if not forbes_entity:
+            from django.contrib import messages
+            messages.error(self.request, 'Forbes Lawn Spraying entity not found!')
+            return self.form_invalid(form)
+        
+        form.instance.entity = forbes_entity
         
         # Generate unique jobber_id for manually created items
         import uuid
@@ -66,6 +94,25 @@ class ServiceItemUpdateView(UpdateView):
     template_name = 'forbes_lawn_accounting/service_item_form.html'
     fields = ['name', 'description', 'category_name', 'default_rate', 'taxable', 'revenue_account', 'active']
     success_url = reverse_lazy('forbes_lawn_accounting:service_item_list')
+    
+    def get_form(self, form_class=None):
+        """Filter revenue account dropdown to Forbes Lawn entity only"""
+        form = super().get_form(form_class)
+        
+        # Get Forbes Lawn entity
+        forbes_entity = EntityModel.objects.filter(
+            name__icontains='Forbes Lawn Spraying'
+        ).first()
+        
+        if forbes_entity:
+            # Filter revenue accounts to this entity and revenue accounts (4024/4025)
+            from django_ledger.models import AccountModel
+            form.fields['revenue_account'].queryset = AccountModel.objects.filter(
+                coa_model__entity=forbes_entity,
+                code__in=['4024', '4025']
+            ).order_by('code')
+        
+        return form
     
     def form_valid(self, form):
         messages.success(self.request, f'Service item "{form.instance.name}" updated successfully!')
